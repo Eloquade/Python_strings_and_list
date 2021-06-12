6eb49ba0e06a82a0d6c4d81c35beb6b21b5cd18d
Lawrence Perez <79312196+Eloquade@users.noreply.github.com>
2021-06-12 08:54:01 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+s ="Hello World"
+print(s)
+
+l = [1,2,3,'A','b','c','d']
+print(l)
+print(l[0])
+print(l[1])
+print(l[2])
+print(l[3])
+print(l[4])
+l.append("Lawrence")
+print(l)
+l.insert(0,"Law")
+print(l)
+l.remove(2)
+print(l)
+print(l)
